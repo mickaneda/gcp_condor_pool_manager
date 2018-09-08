43832f7df4098f212c9bfa2276c9f9bfc7a1cfdd
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2018-09-08 09:05:38 +0000

    fixed logrotate

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 
 cp bin/gcpm /usr/bin/
 echo "/usr/bin/gcpm was installed"
-if [ ! -f /etc/gcpm.conf ];then
-  cp etc/gcpm.conf /etc/
-  echo "/etc/gcpm.conf was installed"
-fi
 cp etc/systemd/system/gcpm.service /etc/systemd/system/
 echo "/etc/systemd/system/gcpm.service was installed"
 cp etc/rsyslog.d/gcpm.conf /etc/rsyslog.d/
@@ -14,6 +10,14 @@ echo "/etc/rsyslog/gcpm.conf was installed"
 systemctl restart rsyslog
 cp etc/logrotate.d/gcpm.conf /etc/logrotate.d/
 echo "/etc/logrotate.d/gcpm.conf was installed"
+
+if [ ! -f /etc/gcpm.conf ];then
+  cp etc/gcpm.conf /etc/
+  echo "/etc/gcpm.conf was installed"
+  echo
+  echo "Please modify /etc/gcpm.conf for your environment"
+  echo
+fi
 
 gcloud=($(grep "^gcloud " /etc/gcpm.conf 2>/dev/null))
 gcloud=${gcloud[1]:-/root/google-cloud-sdk/bin/gcloud}
